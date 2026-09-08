19da20a9159d877e83297efd4e3a1b547ec7f5ec
palgunroy@9999 <drpalgun2007@gmail.com>
2026-09-08 21:13:01 +0530

    ✅ Prime In Diagonal | Easy | C++ | Runtime: 19 ms (5.83%) | Memory: 40.1 MB | 2026-09-08

---
+++ leetcode/easy/2614-prime-in-diagonal/solution.cpp
@@ -0,0 +1,56 @@
+const int N = 4000001; // must cover up to 4*10^6 inclusive
+vector<bool> sa(N, true);
+bool sieveBuilt = false;
+
+void sive()
+{
+    sa[0] = false;
+    sa[1] = false;
+    for (int i = 2; (long long)i * i < N; i++)
+    {
+        if (sa[i])
+        {
+            for (int j = i * i; j < N; j += i)
+            {
+                sa[j] = false;
+            }
+        }
+    }
+}
+
+class Solution {
+public:
+    Solution() {
+        if (!sieveBuilt) {
+            sive();
+            sieveBuilt = true;
+        }
+    }
+    
+    int diagonalPrime(vector<vector<int>>& nums) 
+    {
+        int la = 0;
+        int c = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (sa[nums[i][i]])
+            {
+                c++;
+                la = max(la, nums[i][i]);
+            }
+            int j = n - 1 - i;
+            if (j != i && sa[nums[i][j]])
+            {
+                c++;
+                la = max(la, nums[i][j]);
+            }
+        }
+        if (c == 0)
+        {
+            return 0;
+        }
+        return la;
+    }
+};
