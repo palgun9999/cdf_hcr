7c4c7953d4f459ba380e9d3c69ddac71ff894bf8
palgunroy@9999 <drpalgun2007@gmail.com>
2026-09-14 16:41:12 +0530

    ✅ String Validators | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- hackerrank/medium/string-validators/solution.py
+++ hackerrank/medium/string-validators/solution.py
@@ -1,7 +1,7 @@
-def count_substring(string, sub_string):
-    count = 0
-    for i in range(len(string) - len(sub_string) + 1):
-        if string[i:i + len(sub_string)] == sub_string:
-            count += 1
-    return count 
-
+if __name__ == '__main__':
+    s = input()
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
