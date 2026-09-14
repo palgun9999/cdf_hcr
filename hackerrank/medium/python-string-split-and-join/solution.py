5b2df8b7f9d714ebc0a0db28d4630cfc406bcca3
palgunroy@9999 <drpalgun2007@gmail.com>
2026-09-14 16:38:07 +0530

    ✅ String Split and Join | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- hackerrank/medium/python-string-split-and-join/solution.py
+++ hackerrank/medium/python-string-split-and-join/solution.py
@@ -1,3 +1,8 @@
-def swap_case(s):
-    return s.swapcase()
 
+
+def split_and_join(line):
+    return "-".join(line.split(" "))
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
