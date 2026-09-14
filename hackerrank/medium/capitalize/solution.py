dd5eac120ff9a36d4fa0952d783835f52322add1
palgunroy@9999 <drpalgun2007@gmail.com>
2026-09-14 16:47:27 +0530

    ✅ Capitalize! | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- hackerrank/medium/capitalize/solution.py
+++ hackerrank/medium/capitalize/solution.py
@@ -1,12 +1,6 @@
-import string
 
-def print_rangoli(size):
-    alphabet = string.ascii_lowercase
-    lines = []
-    
-    for i in range(size):
-        s = "-".join(alphabet[i:size])
-        line = (s[::-1] + s[1:]).center(4 * size - 3, "-")
-        lines.append(line)
-        
-    print("\n".join(lines[::-1] + lines[1:]))
+def solve(s):
+    for x in s.split():
+        s = s.replace(x, x.capitalize())
+    return s
+
