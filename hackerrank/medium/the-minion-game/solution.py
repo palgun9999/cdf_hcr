17193af8f79e36825823530b86777c06f86f8e11
palgunroy@9999 <drpalgun2007@gmail.com>
2026-09-14 16:48:04 +0530

    ✅ The Minion Game | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- hackerrank/medium/the-minion-game/solution.py
+++ hackerrank/medium/the-minion-game/solution.py
@@ -1,6 +1,19 @@
-
-def solve(s):
-    for x in s.split():
-        s = s.replace(x, x.capitalize())
-    return s
-
+def minion_game(string):
+    # your code goes here
+    vowels = 'AEIOU'
+    stuart_score = 0
+    kevin_score = 0
+    length = len(string)
+    
+    for i in range(length):
+        if string[i] in vowels:
+            kevin_score += length - i
+        else:
+            stuart_score += length - i
+            
+    if stuart_score > kevin_score:
+        print(f"Stuart {stuart_score}")
+    elif kevin_score > stuart_score:
+        print(f"Kevin {kevin_score}")
+    else:
+        print("Draw")
