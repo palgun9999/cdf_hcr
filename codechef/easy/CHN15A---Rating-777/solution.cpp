3ee4a6c5962d991bf480f9ba7a6754d5f4fd3d25
palgunroy@9999 <drpalgun2007@gmail.com>
2026-09-26 06:48:34 +0530

    ✅ CHN15A - Rating 777 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-26

---
+++ codechef/easy/CHN15A---Rating-777/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int n,k;
+       cin>>n>>k;
+       vector<int> sa(n);
+       for(int i=0;i<n;i++)
+       {
+           cin>>sa[i];
+       }
+       
+   }
+}
